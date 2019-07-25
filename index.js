16f293894276588330cab7d4c357a55ebe1788ab
Andrew C Alvarez <31179195+AndrewCAlvarez@users.noreply.github.com>
2019-07-25 16:25:29 -0400

    Overhaul to design
    -Stuck with one color for design
    -Moved menu to background with content floating in foreground

--- index.js
+++ index.js
@@ -2,14 +2,32 @@ function validateForm() {
   var x = document.forms["myForm"]["fname"].value;
   var y = document.forms["myForm"]["femail"].value;
   var z = document.forms["myForm"]["fmessage"].value;
-  if (x == "") {
+  if (x == "" || x == "Name") {
     alert("Name field is required");
     return false;
-  } else if (y == "") {
+  } else if (y == "" || y == "Email") {
     alert("Email field is required");
     return false;
-  } else if (z == "") {
+  } else if (z == "" || z == "Message...") {
     alert("Message field is required");
     return false;
   }
 }
+
+var fname = document.getElementById("fname");
+
+fname.onclick = function() {
+  fname.value = "";
+};
+
+var femail = document.getElementById("femail");
+
+femail.onclick = function() {
+  femail.value = "";
+};
+
+var fmessage = document.getElementById("fmessage");
+
+fmessage.onclick = function() {
+  fmessage.value = "";
+};
