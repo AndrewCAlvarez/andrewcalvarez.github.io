421379d2bde40689f7da08162707ba1dc0ab603d
Andrew C Alvarez <31179195+AndrewCAlvarez@users.noreply.github.com>
2019-08-02 02:18:29 -0400

    Responsive Nav bar update

--- index.js
+++ index.js
@@ -32,56 +32,12 @@ fmessage.onclick = function() {
   fmessage.value = "";
 };
 
-//Functionality for sliding content over navbar and then having nav bar drop down
-var navBar = document.getElementById("navBar");
-var navBar2 = document.getElementById("navBar2");
-var lastScrollPosition = 0;
-var firstPass = false;
-
-//firstPass is used to determine if the user has scrolled past the navbar. if true,
-//then the second nav bar used as a drop down when a user scrolls up will move behind
-//the first, allowing the user to scroll content over the nav bar again
-//NOTE: CURRENTLY shows both bars on actual mobile devices when scrolling up to top quickly,
-//and when user pulls content down the second bar is shown hiding above the first
-document.body.onscroll = function() {
-  var curScrollPos = document.body.scrollTop;
-  if (curScrollPos < lastScrollPosition) {
-    navBar2.style.display = "block";
-    navBar2.style.zIndex = 1;
-    navBar2.style.top = "0";
-    lastScrollPosition = curScrollPos;
-    if (curScrollPos == 0 && firstPass) {
-      navBar2.style.display = "none";
-      navBar2.style.zIndex = -2;
-      navBar2.style.top = "-50px";
-      firstPass = false;
-    }
-  } else if (curScrollPos > lastScrollPosition && curScrollPos != 0) {
-    navBar2.style.top = "-100px";
-    lastScrollPosition = curScrollPos;
-    navBar2.style.display = "block";
-  }
-  if (curScrollPos > 100) {
-    firstPass = true;
-  }
-};
-
 //hamburger menu functionality. Onclick display = block or none;
 //NOTE: move out of way if user scrolls without need to click
 var hamburgerMenu1 = document.getElementById("hamburgerMenu1");
-var hamburgerMenu2 = document.getElementById("hamburgerMenu2");
 var subMenu = document.getElementById("subMenu");
 var subMenuDisplayed = false;
 hamburgerMenu1.onclick = function() {
-  if (!subMenuDisplayed) {
-    subMenu.style.display = "block";
-    subMenuDisplayed = true;
-  } else {
-    subMenu.style.display = "none";
-    subMenuDisplayed = false;
-  }
-};
-hamburgerMenu2.onclick = function() {
   if (!subMenuDisplayed) {
     subMenu.style.display = "block";
     subMenuDisplayed = true;
